92e1fbe80e1beffa25943ef2369aa5cb30264711
sarah Israel <sarahisrael@Sarahs-MacBook-Pro.local>
2020-05-10 19:06:50 -0400

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
-function splitScroll(){
-    const controller = new ScrollMagic.Controller();
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    function splitScroll(){
+      const controller = new ScrollMagic.Controller();
+  
+      new ScrollMagic.Scene({
+          duration: '300%', 
+          triggerElement: '.title',
+          triggerHook: 0,
+      })
+      .setPin('.title')
+      .addTo(controller);
+  }
+  
+  splitScroll();
+  } else {
 
-    new ScrollMagic.Scene({
-        duration: '300%', 
-        triggerElement: '.title',
-        triggerHook: 0,
-    })
-    .setPin('.title')
-    .addTo(controller);
+  }
 }
 
-splitScroll();
+var x = window.matchMedia("(min-width: 900px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state change
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
